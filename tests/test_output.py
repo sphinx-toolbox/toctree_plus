84dc1cc3b8339da4f9eb0136d7faf5c56aaeee4b
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-09 12:18:15 +0100

    Use sphinx-toolbox.testing for testing output.

--- tests/test_output.py
+++ tests/test_output.py
@@ -7,8 +7,8 @@ import sys
 # 3rd party
 import pytest
 from bs4 import BeautifulSoup  # type: ignore
-from domdf_python_tools.stringlist import StringList
 from pytest_regressions.file_regression import FileRegressionFixture  # type: ignore
+from sphinx_toolbox.testing import check_html_regression
 
 
 def test(app):
@@ -31,23 +31,9 @@ def test_page_38(page: BeautifulSoup, file_regression: FileRegressionFixture):
 
 
 @pytest.mark.skipif(
-		condition=sys.version_info >= (3, 8, 0), reason="Difference in output between 3.6/3.7 and 3.8+"
+		condition=sys.version_info >= (3, 8, 0),
+		reason="Difference in output between 3.6/3.7 and 3.8+",
 		)
 @pytest.mark.parametrize("page", ["index.html", "string.html", "csv.html"], indirect=True)
 def test_page_37(page: BeautifulSoup, file_regression: FileRegressionFixture):
 	check_html_regression(page, file_regression)
-
-
-def remove_html_footer(page: BeautifulSoup) -> BeautifulSoup:
-	for div in page.select("div.footer"):
-		div.extract()
-
-	return page
-
-
-def check_html_regression(page: BeautifulSoup, file_regression: FileRegressionFixture):
-	file_regression.check(
-			contents=str(StringList(remove_html_footer(page).prettify())),
-			extension=".html",
-			encoding="UTF-8",
-			)
