b2f1e75029f16b03fdecc3789142045732f5d001
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-05 08:16:04 +0000

    Updated files with 'repo_helper'.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,22 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"__license__",
-		"__author__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
 		"repo_root",
 		"install_requires",
 		"extras_require",
-		"project_urls",
-
-		"import_name",
 		]
 
 __copyright__ = """
@@ -40,30 +27,7 @@ __copyright__ = """
 """
 
 __version__ = "0.0.4"
-modname = "toctree_plus"
-pypi_name = "toctree_plus"
-import_name = "sphinxcontrib.toctree_plus"
-__license__ = "BSD License"
-short_desc = 'Enhanced Sphinx TocTree that shows classes and functions as if they were sections.'
-__author__ = author = 'Dominic Davis-Foster'
-author_email = 'dominic@davis-foster.co.uk'
-github_username = "domdfcoding"
-web = github_url = "https://github.com/domdfcoding/toctree_plus"
+
 repo_root = pathlib.Path(__file__).parent
 install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
 extras_require = {'all': []}
-
-
-
-conda_description = """Enhanced Sphinx TocTree that shows classes and functions as if they were sections.
-
-
-Before installing please ensure you have added the following channels: domdfcoding, conda-forge"""
-__all__.append("conda_description")
-
-
-project_urls = {
-		"Documentation": "https://toctree_plus.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}
--- doc-source/index.rst
+++ doc-source/index.rst
@@ -11,7 +11,7 @@
 	* - Docs
 	  - |docs| |docs_check|
 	* - Tests
-	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor|
+	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor| |pre_commit_ci|
 	* - PyPI
 	  - |pypi-version| |supported-versions| |supported-implementations| |wheel|
 	* - Anaconda
@@ -99,6 +99,9 @@
 
 .. |pre_commit| pre-commit-shield::
 	:alt: pre-commit
+
+.. |pre_commit_ci| pre-commit-ci-shield::
+	:alt: pre-commit.ci status
 
 .. end shields
 
