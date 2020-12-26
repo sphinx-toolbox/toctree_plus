5b5b154eee8938fafcb86ca631cf0762ab12b164
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-12-26 16:29:16 +0000

    Bump version v0.0.4 -> v0.1.0

--- sphinxcontrib/toctree_plus/__init__.py
+++ sphinxcontrib/toctree_plus/__init__.py
@@ -46,7 +46,7 @@ from sphinx.util import logging
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "BSD"
-__version__: str = "0.0.4"
+__version__: str = "0.1.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["TocTreePlusCollector", "setup"]
