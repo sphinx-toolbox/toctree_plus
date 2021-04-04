ed61ad661f6f3ea6ae743c89b49b68ad22a21e5d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-04-04 22:54:44 +0100

    Test with two different docutils versions

--- tests/test_output.py
+++ tests/test_output.py
@@ -7,6 +7,7 @@ import sys
 # 3rd party
 import pytest
 from bs4 import BeautifulSoup  # type: ignore
+from domdf_python_tools.compat import importlib_metadata
 from pytest_regressions.file_regression import FileRegressionFixture  # type: ignore
 from sphinx_toolbox.testing import check_html_regression
 
@@ -23,17 +24,34 @@ def test(app):
 
 # pytestmark = pytest.mark.sphinx('html', testroot='root')
 
-
-@pytest.mark.skipif(condition=sys.version_info < (3, 8), reason="Difference in output between 3.6/3.7 and 3.8+")
-@pytest.mark.parametrize("page", ["index.html", "string.html", "csv.html"], indirect=True)
-def test_page_38(page: BeautifulSoup, file_regression: FileRegressionFixture):
-	check_html_regression(page, file_regression)
+docutils_version = importlib_metadata.version("docutils")
 
 
-@pytest.mark.skipif(
+def _param(version: str):
+	return pytest.param(
+			version,
+			marks=pytest.mark.skipif(
+					docutils_version != version, reason="Difference in output between docutils versions"
+					)
+			)
+
+
+lt_38 = pytest.mark.skipif(
+		condition=sys.version_info < (3, 8),
+		reason="Difference in output between 3.6/3.7 and 3.8+",
+		)
+
+gte_38 = pytest.mark.skipif(
 		condition=sys.version_info >= (3, 8, 0),
 		reason="Difference in output between 3.6/3.7 and 3.8+",
 		)
+
+
+@pytest.mark.parametrize("py_version", [
+		pytest.param("37", marks=gte_38),
+		pytest.param("38", marks=lt_38),
+		])
+@pytest.mark.parametrize("docutils_version", [_param("0.16"), _param("0.17")])
 @pytest.mark.parametrize("page", ["index.html", "string.html", "csv.html"], indirect=True)
-def test_page_37(page: BeautifulSoup, file_regression: FileRegressionFixture):
+def test_page(py_version: str, docutils_version: str, page: BeautifulSoup, file_regression: FileRegressionFixture):
 	check_html_regression(page, file_regression)
